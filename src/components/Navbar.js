33507ba6f2924ce4c70f217954430d796f9547f8
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-06-08 05:59:25 +0100

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
 import logo from "../images/agromartlogo2.png";
@@ -7,6 +7,11 @@ import { BsCart3 } from "react-icons/bs";
 const Nav = styled("nav")`
   padding: 1.5rem 0;
   background-color: #ffffff;
+  position: relative;
+  &.navbar-expand-sm {
+    background-color: #ffffff;
+    z-index: 1;
+  }
 `;
 
 const LinksWrapper = styled("ul")`
@@ -28,7 +33,14 @@ const Logo = () => {
   );
 };
 
-const NavbarToggler = () => {
+const NavbarToggler = ({ onClick }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+    onClick(!isOpen);
+  };
+
   return (
     <button
       className="navbar-toggler"
@@ -36,17 +48,23 @@ const NavbarToggler = () => {
       data-toggle="collapse"
       data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent"
-      aria-expanded="false"
+      aria-expanded={isOpen ? "true" : "false"}
       aria-label="Toggle navigation"
+      onClick={handleToggle}
     >
-      <span className="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon">
+        <span
+          className="icon-bar"
+          style={{ BackgroundColor: "#004500" }}
+        ></span>
+      </span>
     </button>
   );
 };
 
-const NavLinks = () => {
+const NavLinks = ({ isOpen }) => {
   return (
-    <LinksWrapper className="navbar-nav">
+    <LinksWrapper className={`navbar-nav ${isOpen ? "open" : ""}`}>
       <li className="nav-item active">
         <Link className="nav-link" to="/">
           Home <span className="sr-only">(current)</span>
@@ -77,12 +95,18 @@ const NavLinks = () => {
 };
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = (newState) => {
+    setIsOpen(newState);
+  };
+
   return (
-    <Nav className="navbar navbar-expand-lg px-3">
+    <Nav className="navbar navbar-expand-sm px-3">
       <Logo />
-      <NavbarToggler />
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <NavLinks />
+      <NavbarToggler onClick={handleToggle} />
+      <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
+        <NavLinks isOpen={isOpen} />
       </div>
     </Nav>
   );
